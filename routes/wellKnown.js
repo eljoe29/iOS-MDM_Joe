7f067233879264f3e4001b7eb9c5bd05ae364c6f
eljoe29 <71354073+eljoe29@users.noreply.github.com>
2025-03-18 13:09:12 -0500

    Update wellKnown.js

--- routes/wellKnown.js
+++ routes/wellKnown.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 const knownDomainConfigurations = {
-    "tinslab.us": 'https://manage.microsoft.com/EnrollmentServer/PostReportDeviceInfoForUEV2?aadTenantId=7dbc8268-5fe9-40c8-891f-414a8ff45619'
+    "polpeito.com": 'https://manage.microsoft.com/EnrollmentServer/PostReportDeviceInfoForUEV2?aadTenantId=cbd365af-ca5f-42f3-ad61-5ad2873f5431'
 }
 
 const knownUserNameOverrides = {
